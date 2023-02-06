31ef7be904d67947b4970970ccd7735af83755b2
Hhummann <hhummannv@gmail.com>
2023-02-06 01:16:47 +0100

    Added stats info image generator

--- imageProcessing.py
+++ imageProcessing.py
@@ -1,5 +1,56 @@
 from PIL import Image, ImageDraw, ImageFont
 from style import color
+from stats import elements
+
+
+def infoPrint(card, element):
+    e = elements
+    text = ImageDraw.Draw(card)
+
+    font = ImageFont.truetype('data/fonts/nick.ttf', size=35)
+    text.text((15, 10), element.upper(), fill=color[f"{element}"], font=font)
+
+    font = ImageFont.truetype('data/fonts/nick.ttf', size=15)
+    for y, t in zip([110, 160, 210, 260, 310], ["Shield:", "Charge damage:", "Attack:", "Energy:", "Health:"]):
+        text.text((15, y), t, fill=color[f"{element}"], font=font)
+
+    for x, t in zip([170, 270, 370, 470], ["Mask", "Normal", "Heal", "Rest"]):
+        text.text((x, 70), t, fill=color[f"{element}"], font=font)
+
+    font = ImageFont.truetype('data/fonts/nick.ttf', size=18)
+    for x, n in zip([170, 270, 370, 470], ["mask", "normal", "heal", "rest"]):
+        for y, t in zip([110, 160, 210, 260, 310], [e[element][n]['shield'],
+                                                   e[element][n]['damage'],
+                                                   e[element][n]['attack'],
+                                                   e[element][n]['energy'],
+                                                   e[element][n]['health']]):
+            text.text((x, y), f"{t}", fill=color[f"{element}"], font=font)
+
+def infoCard(element):
+    card = Image.open(f'data/bg/{element}-bg.jpg')
+
+    card = card.copy()
+    infoPrint(card, element)
+
+    card.save(f'data/cards/{element}.jpg', quality=95)
+
+
+def initInfo():
+    for element in ["fire", "water", "earth", "stone", "air", "ice"]:
+        infoCard(element)
+
+    mergeInfo()
+
+
+def mergeInfo():
+    merge = Image.new('RGB', (1082, 1080))
+    array = []
+    for element in ["fire", "water", "earth", "stone", "air", "ice"]:
+        array.append(Image.open(f"data/cards/{element}.jpg"))
+    for i in range(6):
+        merge.paste( array[i], ( (i%2)*541, (i%3)*360) )
+
+    merge.save("data/cards/info.jpg")
 
 
 def addBars(card, player):
@@ -39,12 +90,10 @@ def card(player):
     card.save(f'data/cards/{player.name}.jpg', quality=95)
 
 
-def get_concat_v(p1, p2):
-    merge = Image.new('RGB', (541, 680))
+def merge(p1, p2):
+    merge = Image.new('RGB', (541, 720))
     i1 = Image.open(f'data/cards/{p1.name}.jpg')
     i2 = Image.open(f'data/cards/{p2.name}.jpg')
     merge.paste(i1, (0, 0))
-    merge.paste(i2, (0, 340))
+    merge.paste(i2, (0, 360))
     merge.save(f'data/cards/{p1.name}-{p2.name}.jpg', quality=95)
-
-
