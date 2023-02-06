a6deaf817beb4fe8b4a044e73e9ca85791b18798
Hhummann <hhummannv@gmail.com>
2023-02-06 01:25:20 +0100

    Optimised text generation

--- imageProcessing.py
+++ imageProcessing.py
@@ -55,10 +55,12 @@ def mergeInfo():
 
 def addBars(card, player):
     paint = ImageDraw.Draw(card)
-    paint.rectangle([(15, 130), (15 + player.health * 2, 150)], fill="#00A300", outline="green")
-    paint.rectangle([(15, 180), (15 + player.energy * 2, 200)], fill="#880ED4", outline="purple")
-    paint.rectangle([(15, 230), (15 + player.shield * 2, 250)], fill="#2E2EFF", outline="blue")
-    paint.rectangle([(15, 280), (15 + player.damage * 2, 300)], fill="#FF2E2E", outline="red")
+    for y, l, t, c, o in zip([130, 180, 230, 280],
+                       [150, 200, 250, 300],
+                       [player.health, player.energy, player.shield, player.damage],
+                       ["#00A300", "#880ED4", "#2E2EFF", "#FF2E2E"],
+                       ["green", "purple", "blue", "red"]):
+        paint.rectangle([(15, y), (15 + t * 2, l)], fill=c, outline=o)
 
 def addText(card, player):
     text = ImageDraw.Draw(card)
@@ -66,15 +68,11 @@ def addText(card, player):
     text.text((110, 10), player.name, fill=color[f"{player.element}"], font=font)
 
     font = ImageFont.truetype('data/fonts/nick.ttf', size=15)
-    text.text((15, 110), "Health", fill=color[f"{player.element}"], font=font)
-    text.text((15, 160), "Energy", fill=color[f"{player.element}"], font=font)
-    text.text((15, 210), "Shield", fill=color[f"{player.element}"], font=font)
-    text.text((15, 260), "Damage", fill=color[f"{player.element}"], font=font)
-    font = ImageFont.truetype('data/fonts/nick.ttf', size=15)
-    text.text((17, 130), f"{player.health}", fill=color[f"{player.element}"], font=font)
-    text.text((17, 180), f"{player.energy}", fill=color[f"{player.element}"], font=font)
-    text.text((17, 230), f"{player.shield}", fill=color[f"{player.element}"], font=font)
-    text.text((17, 280), f"{player.damage}", fill=color[f"{player.element}"], font=font)
+    for y, t in zip([110, 160, 210, 260], ["Health", "Energy", "Shield", "Damage"]):
+        text.text((15, y), t, fill=color[f"{player.element}"], font=font)
+
+    for y, s in zip([130, 180, 230, 280], [player.health, player.energy, player.shield, player.damage]):
+        text.text((17, y), f"{s}", fill=color[f"{player.element}"], font=font)
 
 
 def card(player):
