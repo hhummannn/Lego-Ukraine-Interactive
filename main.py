a30099add54e8154aa6dfe2b075df7d53db7f792
Hhummann <hhummannv@gmail.com>
2023-02-10 12:22:47 +0100

    Custom help, edited gameinfo, rearranged functions

--- main.py
+++ main.py
@@ -146,7 +146,6 @@ class Game:
 
         move = moves_[str(reaction)]
 
-        print(move)
 
         # give up
         if move == "give up":
@@ -194,28 +193,6 @@ async def gameTime(p1, e1, p2, e2, channel):
     return True
 
 
-
-# commands start here
-@bot.command()
-async def test(ctx):
-    await ctx.channel.send("I live!")
-
-
-
-@bot.command()
-async def gameinfo(ctx):
-    await ctx.channel.send(f"This is 2-man Bionicle inspired step-by-step game (name is to be added)\n\n"
-                           f"Rules:\n"
-                           f"1. Two players confirm their participation by stating their element for the game.\n"
-                           f"2. Players make their move one after another.\n"
-                           f"Make sure to read what options you have. Trying to make a move that takes more stamina than you have will be punished.\n"
-                           f"3. Players' stats are updated after each person's move.\n"
-                           f"4. Each player can give up on their move by writing 'give up'.\n"
-                           f"5. The game is ended as soon as someone's health reaches zero.\n\n"
-                           f"Have fun!")
-
-
-
 async def playerEntry(ctx):
     def check(reaction, user):
         return str(reaction) in ["🔥", "💧", "🌄", "🪨", "🌪", "❄"] and user.id != bot.user.id
@@ -239,11 +216,33 @@ async def playerEntry(ctx):
         "❄": "ice",
     }
     await ctx.channel.send(f"Player confirmed: {user.name}, {els[str(reaction)]}")
-    avatar = ctx.author.display_avatar.url
+    avatar = user.display_avatar.url
     data = requests.get(avatar).content
     with open(f"data/avatar/{user.name}.jpg", 'wb') as handler:
         handler.write(data)
     return user, els[str(reaction)]
+
+
+
+
+
+
+# commands start here
+@bot.command()
+async def help(ctx):
+    await ctx.channel.send(f"This is Lego Ukraine Interactive bot (LUI), that is created for Lego Ukraine discord server\n"
+                           f"The bot is in development, so wait for updates!\n\n"
+                           f"Available commands are:\n\n"
+                           f"!help - show this message\n"
+                           f"!test - check if the bot is alive (not guaranteed for now...)\n"
+                           f"!element - get information about a specific element of Bionicle Game\n"
+                           f"!gameinfo - get information and rules about Bionicle 1v1 game\n"
+                           f"!game - start the game")
+
+
+@bot.command()
+async def test(ctx):
+    await ctx.channel.send("I live!")
 
 
 @bot.command()
@@ -251,7 +250,8 @@ async def element(ctx):
     def check(reaction, user):
         return str(reaction) in ["🔥", "💧", "🌄", "🪨", "🌪", "❄"] and user == ctx.author
 
-    choiceMessage = await ctx.channel.send("Choose element to get info about")
+    choiceMessage = await ctx.channel.send("Choose element to get info about:\n"
+                           f"Fire - 🔥\nWater - 💧\nEarth - 🌄\nStone - 🪨\nAir - 🌪\nIce - ❄")
     for emoji in ["🔥", "💧", "🌄", "🪨", "🌪", "❄"]:
         await choiceMessage.add_reaction(emoji)
     while True:
@@ -267,7 +267,7 @@ async def element(ctx):
         "❄": "ice",
     }
     elinfo = els[str(reaction)]
-    await ctx.channel.send(f"{elinfo} abilities:\n"
+    await ctx.channel.send(f"{elinfo[0].upper() + elinfo[1:]} abilities:\n"
                        f"1. Mask - {elements[elinfo]['mask']['name']}: {elements[elinfo]['mask']['description']}\n\n"
                        f"2. Normal attack - {elements[elinfo]['normal']['name']}: {elements[elinfo]['normal']['description']}\n\n"
                        f"3. Healing ability - {elements[elinfo]['heal']['name']}: {elements[elinfo]['heal']['description']}\n\n"
@@ -276,15 +276,17 @@ async def element(ctx):
 
 
 @bot.command()
-async def help(ctx):
-    await ctx.channel.send(f"This is Lego Ukraine Interactive bot (LUI), that is created for Lego Ukraine discord server\n"
-                           f"The bot is in development, so wait for updates!\n\n"
-                           f"Available commands are:\n\n"
-                           f"!help - show this message\n"
-                           f"!test - check if the bot is alive (not guaranteed for now...)\n"
-                           f"!element - get information about a specific element of Bionicle Game\n"
-                           f"!gameinfo - get information and rules about Bionicle 1v1 game\n"
-                           f"!game - start the game")
+async def gameinfo(ctx):
+    await ctx.channel.send(f"This is 2-man Bionicle inspired step-by-step game (name to be added)\n\n"
+                           f"Rules:\n"
+                           f"1. Two players confirm their participation by stating their element for the game.\n"
+                           f"2. Players make their move one after another.\n"
+                           f"Make sure to read what options you have. Trying to make a move that takes more stamina than you have will be punished.\n"
+                           f"3. Players' stats are updated after each person's move.\n"
+                           f"4. The game is ended as soon as someone's health reaches zero.\n\n"
+                           f"Have fun!")
+
+
 
 
 @bot.command()
